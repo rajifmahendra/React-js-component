f77306b331066a099c297a4504ccdc3237ab7265
rajifmahendra <rajifmahendra10@gmail.com>
2022-05-29 21:13:19 +0700

    update

---
+++ src/components/movies.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+    state = {
+        movies: getMovies()
+    };
+
+    handleDelete = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({ movies })
+        // console.log(movie)
+    }
+
+    render() {
+        const { lenght: count } = this.state.movies;
+        if( count === 0)
+            return <p>There are no movies in the database</p>;
+    
+        return (
+            <>
+            <p>Showing {count} movies in the database</p>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Genre</th>
+                        <th scope="col">Stock</th>
+                        <th scope="col">Rate</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.state.movies.map(movie => (
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                        </tr>
+                    )) }
+                    
+                </tbody>
+            </table>
+            </>
+          )
+    }
+}
+
+export default Movies
